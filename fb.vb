4b7866e49fe4d2d11660f006c3dbbf74c58c1b94
DENNISK\denniskon <denkon28@gmail.com>
2017-04-20 21:57:52 +0300

    fix

---
+++ fb.vb
@@ -0,0 +1,6 @@
+﻿Public Class fb
+
+    Private Sub fb_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        WebBrowser1.Navigate("http://www.facebook.com/sharer.php?u=")
+    End Sub
+End Class
